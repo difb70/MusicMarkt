211817088580cd1c011724bf2e430991c49448ae
difb70 <difb70@gmail.com>
2022-12-04 11:18:30 -0500

    Small update to server/README.md and started app.py. Client can perform login and register commnands (not communicating with database yet)

--- Server/README .md
+++ Server/README .md
@@ -1,6 +1,9 @@
 # SIRS
 
-In order to run the server, you must execute the following command:\
+In order to run the server, you must execute the following commands:\
     - _export FLASK_APP=app_\
     - _export FLASK_ENV=development_\
     - _flask run --host=10.0.2.15_
+
+Besides that, it is needed to add the following port forwarding rule to the server NAT Network:\
+    -_&emsp; Rule 2 &emsp; TCP &emsp; 127.0.0.1 &emsp; 5000 &emsp; 10.0.2.15 &emsp; 5000_
--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,86 @@
 from flask import Flask
+from flask import render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+#################################################################################
+#
+#                               Initial Page
+#
+#################################################################################
 @app.route('/')
-def hello():
-    return '<h1>Hello, World!</h1>'
+def initialPage():
+    try:
+        return render_template("initialPage.html")
+    except Exception as e:
+        return render_template("error.html", error_message = e)  # Renders a page with the error.
+
+
+#################################################################################
+#
+#                               Register Page
+#
+#################################################################################
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    error = None
+    if request.method == 'POST':
+
+        #(TODO) verify if that email or that username is already registered in the database'
+
+        #(TODO) else, redirect him to 'menu'
+
+        if request.form['email'] != 'admin@gmail.com' or request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        
+        else:
+            print("email: ", request.form['email'])
+            print("username: ", request.form['username'])
+            print("password: ", request.form['password'])
+            
+            return redirect(url_for('menu'))\
+
+    return render_template('register.html', error=error)
+
+
+#################################################################################
+#
+#                               Login Page
+#
+#################################################################################
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+
+        #(TODO) if user is not registered in database, return 'Invalid Credentials. Please try again.'
+
+        #(TODO) else, redirect him to 'menu'
+
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        
+        else:
+            
+            print("username: ", request.form['username'])
+            print("password: ", request.form['password'])
+            
+            return redirect(url_for('menu'))\
+
+    return render_template('login.html', error=error)
+
+
+#################################################################################
+#
+#                               Menu Page
+#
+#################################################################################
+@app.route('/menu')
+def menu():
+    try:
+        return render_template("menu.html")
+    except Exception as e:
+        return render_template("error.html", error_message = e)  # Renders a page with the error.
 
 if __name__ == '__main__':
 	app.run(debug=True)
