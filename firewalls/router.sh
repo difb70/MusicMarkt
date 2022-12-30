b3f264c2fe0b1b0379b0504ab8d8fe957d68a4b9
Th0mz <tomastavares21122001@gmail.com>
2022-12-30 13:48:15 -0500

    internal and external firewalls done

--- firewalls/router.sh
+++ firewalls/router.sh
@@ -1,18 +1,36 @@
+#!/bin/sh
+API="192.168.0.100"
+API_PORT="5000"
+DMZ="192.168.0.0/24"
+
+DATABASE="192.168.1.1"
+DB_PORT="5432"
+INT_NET="192.168.1.0/24"
+
 # clean previous configuration
 sudo iptables -F
+sudo iptables -t nat -F
 
-# set default policy of forward chain to be accept
-# final version will be drop (just allowing the connections
-# specified by the rules)
+# set all chains to a whitelisting stratagy
 sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT DROP
 
-# external firewall rules
+### external firewall rules ###
 
-# internal firewall rules
+# External <--> API connection
+
+sudo iptables -A FORWARD -d $API -p tcp --dport $API_PORT -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+# > route packets destined to the router on port 5000 to the API server
+sudo iptables -t nat -A PREROUTING -i enp0s9 -p tcp -j DNAT --to-destination "${API}:${API_PORT}"
+
+###############################
 
 
-# internet access
-sudo iptables -t nat -F
-sudo iptables -t nat -A POSTROUTING  -o enp0s9 -j MASQUERADE
+### internal firewall rules ###
+
+# API <--> Database connection
+sudo iptables -A FORWARD -s $API -d $DATABASE -p tcp --dport $DB_PORT -m state --state NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -s $DATABASE -d $API -p tcp --sport $DB_PORT -m state --state ESTABLISHED -j ACCEPT
+
+##############################
