59999d878a296c182e5f5ca6df698c4a56ac7d36
WhoIsGinja <joao.ginja.ramalho@tecnico.ulisboa.pt>
2023-01-02 23:37:46 +0000

    encrytion and hashin working needs fixes on the amount of messages sent

--- client/mobileApp.py
+++ client/mobileApp.py
@@ -1,21 +1,18 @@
 import socket
 import rsa
+import hmac
+import hashlib
 from cryptography.fernet import Fernet
 
 HOST = "127.0.0.1"
 PORT = 65432
-
-# encrypt client -> server : privClient -> pubAPI
-# decrypt server -> client : privClient -> pubAPI
-
-# encriptar com a publica do outro, e depois com o secret
 
 def encryptPublicAPI(msg, publicKey):
     return rsa.encrypt(msg.encode('utf-8'), publicKey)
 
 def decryptPrivateClient(cyphertext, privateKey):
     try:
-        return rsa.decrypt(cyphertext, privateKey).decode('utf-8')
+        return rsa.decrypt(cyphertext, privateKey)
     except:
         return False
 
@@ -23,15 +20,33 @@ def loadSecretKey():
 
     return open("client_keys/secretKey.key", "rb").read()
 
+def hash_hmac(key, message):
+    hash_object = hmac.new(key.encode(), message.encode(), hashlib.sha256)
+
+    hmac_value = hash_object.digest()
+
+    return hmac_value
+
+def check_hash(key, messageBytes, hash):
+    hash_object = hmac.new(key.encode(), messageBytes, hashlib.sha256)
+
+    hmac_value = hash_object.digest()
+
+    if(hmac_value == hash):
+        return True
+    else:
+        return False
 
 
-def verifyMessage(message, privateKey, sharedKey):
-    #TODO message should be decrypted here and also check for integrity and authenticity
-    #TODO if everything checks out
+def verifyMessage(message, privateKey, sharedKey, secret_hash):
+
+    #separate the encypted stuff from the hash
+    hashed = message[-32:]
+    messageEncrypted = message[:-32]
 
     #shared key decryption
     key_fernet_alg = Fernet(sharedKey)
-    decryptedSecret = key_fernet_alg.decrypt(message)
+    decryptedSecret = key_fernet_alg.decrypt(messageEncrypted)
 
     responseEncrypted = decryptedSecret
     
@@ -40,11 +55,16 @@ def verifyMessage(message, privateKey, sharedKey):
     if(decryptedPrivate == False):
         return False
 
-    #TODO check integrity
+    hashes_match = check_hash(secret_hash, decryptedPrivate, hashed)
 
-    print(decryptedPrivate)
-    
-    return True
+    if(hashes_match):
+        print("2FA code: " + decryptedPrivate.decode('utf-8'))
+
+        return True
+    else:
+        print("ERROR: problems with the communication channel")
+
+        return False
 
 
 
@@ -55,6 +75,8 @@ with open("client_keys/apiPublic.pem", "rb") as f:
     apiPublicKey = rsa.PublicKey.load_pkcs1(f.read())
 
 secretKey = loadSecretKey()
+
+secret_hash = "A09-alameda"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
@@ -71,27 +93,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                     data = conn.recv(1024)
                     if not data:
                         break
-                    
-                    decoded = data.decode('utf-8')
 
-                    #verifymessage, decrypt and print message received by the server
-
-                    message = verifyMessage(data, clientPrivateKey, secretKey)
-                    message = True
+                    message = verifyMessage(data, clientPrivateKey, secretKey, secret_hash)
                     
                     if (message != False):
                         response = "OK"
                     else:
                         response = "NOK"
-                    
-                    # TODO response should be encrypted (guarantees confidentiality, integrity and authenticity)
 
-                    #TODO integrity
+                    hash_to_send = hash_hmac(secret_hash, response)
 
                     encryptedPublicResponse = encryptPublicAPI(response, apiPublicKey)
 
                     key_fernet = Fernet(secretKey)
                     encryptedResponse = key_fernet.encrypt(encryptedPublicResponse)
+
+                    encryptedResponse = encryptedResponse + hash_to_send
 
                     conn.sendall(encryptedResponse)
         except socket.timeout:
