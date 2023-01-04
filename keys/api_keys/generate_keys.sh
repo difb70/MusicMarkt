29d8282c48d481306890b8a276bd0e0950239359
Th0mz <tomastavares21122001@gmail.com>
2023-01-04 11:15:25 -0500

    generate key files added

--- keys/api_keys/generate_keys.sh
+++ keys/api_keys/generate_keys.sh
@@ -5,6 +5,5 @@ rm -fr *.csr
 
 echo "generating https cetificate ..."
 openssl genrsa -out api_server.key.pem 4096
-openssl req -new -config configuration/api.cnf -key api_server.key.pem -out api_server.csr
-openssl x509 -req -days 365 -in api_server.csr -CA ../ca_keys/ca.cert.pem -CAkey ../ca_keys/ca.key.pem -out api_server.cert.pem
-
+openssl req -new -nodes -config configuration/api.cnf -key api_server.key.pem -out api_server.csr
+openssl x509 -req -days 365 -in api_server.csr -CA ../ca_keys/ca.cert.pem -CAkey ../ca_keys/ca.key.pem -CAcreateserial -out api_server.cert.pem
--- keys/ca_keys/generate_keys.sh
+++ keys/ca_keys/generate_keys.sh
@@ -5,6 +5,6 @@ rm -fr *.csr
 
 echo "generating root CA ..."
 openssl genrsa -out ca.key.pem 4096
-openssl req -config configuration/ca.cnf -new -key ca.key.pem -out ca.csr
+openssl req -new -nodes -config configuration/ca.cnf -key ca.key.pem -out ca.csr
 openssl x509 -req -days 365 -in ca.csr -signkey ca.key.pem -out ca.cert.pem
 echo 01 > ca.cert.srl
