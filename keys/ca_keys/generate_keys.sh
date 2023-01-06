1268d181910057f4ca292927ca0dfad754845f51
Tomás Tavares <57302012+Th0mz@users.noreply.github.com>
2023-01-06 14:23:31 +0000

    Update generate_keys.sh

--- keys/ca_keys/generate_keys.sh
+++ keys/ca_keys/generate_keys.sh
@@ -8,5 +8,5 @@ echo "generating root CA ..."
 sudo openssl req -new -nodes -text -config configuration/ca.cnf \
     -out root_ca.csr -keyout root_ca.key
 sudo chmod og-rwx root_ca.key
-sudo openssl x509 -req -in root_ca.csr -text -days 365 \
+sudo openssl x509 -req -in root_ca.csr -text -days 1095 \
     -signkey root_ca.key -out root_ca.crt
